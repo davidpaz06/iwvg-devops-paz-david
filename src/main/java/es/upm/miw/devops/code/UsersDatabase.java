ed02a1011703f1075c324cc7c30424f498016235
davidpaz06 <davidpaz.dev@gmail.com>
2026-09-11 16:50:19 +0200

    Updated user with id = 6

--- src/main/java/es/upm/miw/devops/code/UsersDatabase.java
+++ src/main/java/es/upm/miw/devops/code/UsersDatabase.java
@@ -46,7 +46,7 @@ public class UsersDatabase {
                 new User("3", "Oscar", "López", fractions3),
                 new User("4", "Paula", "Torres", fractions4),
                 new User("5", "Antonio", "Blanco", fractions5),
-                new User("6", "Paula", "Torres", fractions6)
+                new User("6", "David", "Paz", fractions6)
         );
     }
 }
